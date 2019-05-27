1ca35c78a78a07aeb3605c05c9f95e470cdf44ee
Tadas Maslauskas <maslauskas.t@gmail.com>
2019-05-27 13:25:50 +0300

    refactor: extracted common code to setUp method

--- tests/SignatureGeneratorTest.php
+++ tests/SignatureGeneratorTest.php
@@ -2,48 +2,55 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase;
 use Maslauskas\MoneyMatrixClient\SignatureGenerator;
+use PHPUnit\Framework\TestCase;
 
 class SignatureGeneratorTest extends TestCase
 {
-    /** @test */
-    public function encode_signature()
+    /**
+     * @var SignatureGenerator
+     */
+    private $generator;
+
+    protected function setUp(): void
     {
+        parent::setUp();
         $merchantId = "1008";
         $merchantKey = "066B1741D3D111E59E9846218491E2A3";
 
+        $this->generator = new SignatureGenerator($merchantId, $merchantKey);
+    }
+
+    /** @test */
+    public function encode_signature()
+    {
         $data = [
             'MerchantReference' => '3c05bea5191d4ff085216b100ca2183f',
             'PaymentMethod' => 'CreditCard',
             'CustomerId' => '1008~121223',
             'Amount' => '102.00',
-            'Currency' => 'USD'
+            'Currency' => 'USD',
         ];
 
-        $signature = new SignatureGenerator($merchantId, $merchantKey);
         $expected = "IOWi0IyH74nU7O0Da8WngNMXUviUcsi72AL19epTObl7dy6RX41e2KHBJC0LzmDAO8BENlJd1KYTQrwCS2ofpA==";
+        $signature = $this->generator->generate($data);
 
-        self::assertTrue(hash_equals($expected, $signature->generate($data)), "Hashes are not equal!");
+        self::assertTrue(hash_equals($expected, $signature), "Hashes are not equal!");
     }
 
     /** @test */
     public function validate_signature()
     {
-        $merchantId = "1008";
-        $merchantKey = "066B1741D3D111E59E9846218491E2A3";
-
         $data = [
             'MerchantReference' => '3c05bea5191d4ff085216b100ca2183f',
             'PaymentMethod' => 'CreditCard',
             'CustomerId' => '1008~121223',
             'Amount' => '102.00',
-            'Currency' => 'USD'
+            'Currency' => 'USD',
         ];
 
-        $generator = new SignatureGenerator($merchantId, $merchantKey);
         $signature = "IOWi0IyH74nU7O0Da8WngNMXUviUcsi72AL19epTObl7dy6RX41e2KHBJC0LzmDAO8BENlJd1KYTQrwCS2ofpA==";
 
-        self::assertTrue($generator->validate($signature, $data));
+        self::assertTrue($this->generator->validate($signature, $data));
     }
 }
